ed90391d08386d9722150305876b7f0288f2baf8
Jake Robins <mendahu@gmail.com>
2020-01-14 20:26:16 +0000

    fixed linting

--- spinner1.js
+++ spinner1.js
@@ -1,20 +1,20 @@
 let symbols = [
-  '\r|   ', 
-  '\r/   ', 
+  '\r|   ',
+  '\r/   ',
   '\r-   ',
   '\r\\   ',
-  '\r|   ', 
-  '\r/   ', 
+  '\r|   ',
+  '\r/   ',
   '\r-   ',
   '\r\\   ',
-  ]
+];
   
   
-  let time = 100;
-  for(let i = 0; i < symbols.length; i++){
+let time = 100;
+for (let i = 0; i < symbols.length; i++) {
   setTimeout(() => {
     process.stdout.write(symbols[i]);
-  }, time + (i*200))
+  }, time + (i * 200));
   
 }
 
