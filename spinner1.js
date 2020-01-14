56caa1ef7dc0e83fb10e9574af5954b221100b18
Olivia <oliviapower.02@gmail.com>
2020-01-14 20:36:42 +0000

    add spinner functionality

---
+++ spinner1.js
@@ -0,0 +1,21 @@
+let symbols = [
+  '\r|   ', 
+  '\r/   ', 
+  '\r-   ',
+  '\r\\   ',
+  '\r|   ', 
+  '\r/   ', 
+  '\r-   ',
+  '\r\\   ',
+  ]
+  
+  
+  let time = 100;
+  for(let i = 0; i < symbols.length; i++){
+  setTimeout(() => {
+    process.stdout.write(symbols[i]);
+  }, time + (i*200))
+  
+}
+
+
